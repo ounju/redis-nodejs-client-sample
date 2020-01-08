36cef9edafbed84048da9375f01fba9c1143a8c1
ounju <kimounju@gmail.com>
2020-01-08 18:58:24 +0900

    andy

--- redis-test.sh
+++ redis-test.sh
@@ -2,7 +2,7 @@
 while [ true ]
 do
   echo "`date` ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-  # redis-cli -p 26379 -h andyredis -a redispassword SENTINEL get-master-addr-by-name mymaster
-  node sentineltest.js
+  redis-cli -p 26379 -h aredis-dev.opsnow.io -a redispassword SENTINEL get-master-addr-by-name mymaster
+  node redistest.js
   sleep 1
 done
